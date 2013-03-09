6f962396a94ced4714160e683e180782a044b280
samsung.myspc@gmail.com <samsung.myspc@gmail.com@6cc6611d-b7c0-ba1b-36a4-0882d911e875>
2013-03-09 15:31:38 +0000

    memory part is not real circuit emulation

--- trunk/source/src/vm/fc100/memory.cpp
+++ trunk/source/src/vm/fc100/memory.cpp
@@ -1,6 +1,4 @@
 /*
-	SANYO PHC-25 Emulator 'ePHC-25'
-	SEIKO MAP-1010 Emulator 'eMAP-1010'
 	Skelton for retropc emulator
 
 	Author : Takeda.Toshiya
@@ -10,6 +8,7 @@
 */
 
 #include "memory.h"
+#include "../../config.h"
 #include "../../fileio.h"
 
 #define SET_BANK(s, e, w, r) { \
@@ -32,72 +31,158 @@
 
 void MEMORY::initialize()
 {
-	memset(rom, 0xff, sizeof(rom));
 	memset(rdmy, 0xff, sizeof(rdmy));
-	
+	// set memory map
+	SET_BANK(0x0000, 0x5fff, wdmy,rom); // 32KB
+	SET_BANK(0x6000, 0x7fff, wdmy,extrom); // 8KB
+	SET_BANK(0x8000, 0xbfff, extram, extram); // 32KB
+	SET_BANK(0xc000, 0xffff, ram, ram); // 32KB
+}
+
+void MEMORY::readrom(uint32 data)
+{
+	ramsel = (data & 1) != 0; // ext ram: 1, ext rom : 2
 	// load rom image
 	FILEIO* fio = new FILEIO();
-	if(fio->Fopen(emu->bios_path(_T("BASIC.ROM")), FILEIO_READ_BINARY)) {
-		fio->Fread(rom, sizeof(rom), 1);
+#ifdef _IO_DEBUG_LOG
+	emu->out_debug(_T("Loading...\n"));
+#endif
+	if(fio->Fopen(emu->bios_path(_T("FC100U48.ROM")), FILEIO_READ_BINARY)) {
+		fio->Fread(rom, 0x2000, 1);
 		fio->Fclose();
+		if (getcrc32(rom, 0x2000) != 0x24E78E75) memset(rom, 0xff, 0x2000);
+	}
+#ifdef _IO_DEBUG_LOG
+	else emu->out_debug(_T("FC100U48.ROM Fail.\n"));
+#endif
+
+	if(fio->Fopen(emu->bios_path(_T("FC100U49.ROM")), FILEIO_READ_BINARY)) {
+		fio->Fread(&rom[0x2000], 0x2000, 1);
+		fio->Fclose();
+		if (getcrc32(rom + 0x2000, 0x2000) != 0xE14FC7E9) memset(rom + 0x2000, 0xff, 0x2000);
+	}
+#ifdef _IO_DEBUG_LOG
+	else emu->out_debug(_T("FC100U49.ROM Fail.\n"));
+#endif
+
+	if(fio->Fopen(emu->bios_path(_T("FC100U50.ROM")), FILEIO_READ_BINARY)) {
+		fio->Fread(&rom[0x4000], 0x2000, 1);
+		fio->Fclose();
+		if (getcrc32(rom + 0x4000, 0x2000) != 0xD783C84E) memset(rom + 0x4000, 0xff, 0x2000);
+	}
+#ifdef _IO_DEBUG_LOG
+	else emu->out_debug(_T("FC100U50.ROM Fail.\n"));
+#endif
+
+	if (data & 2) {// extrom on : type = 2
+#ifdef _IO_DEBUG_LOG
+		emu->out_debug(_T("Checking...\n"));
+#endif
+		if(fio->Fopen(emu->bios_path(_T("FC100EXT.ROM")), FILEIO_READ_BINARY)) {
+			fio->Fread(extrom, 0x2000, 1);
+			fio->Fclose();
+#ifdef _IO_DEBUG_LOG
+			if (extrom[0] != 0x47 && extrom[1] != 0x53) {
+				emu->out_debug(_T("GS-ROM OK."));
+			}
+			else if (extrom[0x8c2] != 0xd8) {
+				emu->out_debug(_T("EXTROM OK."));
+			}
+			else{
+				emu->out_debug(_T("Fail. (Unknown ROM)"));
+				memset(extrom, 0xff, 0x2000);
+			}
+#endif
+		}
+#ifdef _IO_DEBUG_LOG
+		else emu->out_debug(_T("FC100EXT.ROM Fail.\n"));
+#endif
+	}
+
+	if(fio->Fopen(emu->bios_path(_T("FC100U53.ROM")), FILEIO_READ_BINARY)) {
+		fio->Fread(cgrom, 0x1000, 1);
+		fio->Fclose();
+		if (getcrc32(cgrom, 0x1000) != 0x2DE75B7F) {
+			memset(cgrom, 0xff, 0x1000);
+		}
+	}
+	else{
+#ifdef _IO_DEBUG_LOG
+		emu->out_debug(_T("FC100U53.ROM Fail.\n"));
+#endif
+		memset(cgrom, 0xff, sizeof(cgrom));
 	}
 	delete fio;
-	
-	// set memory map
-#ifdef _MAP1010
-	SET_BANK(0x0000, 0x5fff, wdmy, rom );
-	SET_BANK(0x6000, 0x77ff, vram, vram);
-	SET_BANK(0x7800, 0x7fff, wdmy, rdmy);
-	SET_BANK(0x8000, 0xffff, ram,  ram );
-#else
-	//SET_BANK(0x0000, 0x5fff, wdmy, rom );
-	//SET_BANK(0x6000, 0x77ff, vram, vram);
-	//SET_BANK(0x7800, 0xbfff, wdmy, rdmy);
-	//SET_BANK(0xc000, 0xffff, ram,  ram );
+}
 
+void MEMORY::write_io8(uint32 addr, uint32 data)
+{
+	// warning : this part is not real circuit emulation
 
-	SET_BANK(0x0000, 0x5fff, wdmy, rom );
-	SET_BANK(0x6000, 0xbfff, wdmy, rdmy);
-	SET_BANK(0xc000, 0xc7ff, vram, vram);
-	SET_BANK(0xc800, 0xffff, ram,  ram );
-
+	addr &= 0xff;
+	switch (addr) {
+	case 0x70:
+		if (~romsel & 0x10)
+		{
+			SET_BANK(0x6000, 0x7fff, wdmy, extrom);
+			romsel = 0x70;
+#ifdef _IO_DEBUG_LOG
+			emu->out_debug(_T("Working EXTROM\n"));
 #endif
+		}
+		break;
+	case 0x71:
+		romsel = 0x71;
+		break;
+	case 0x60:
+		if (romsel == 0x71) {
+			SET_BANK(0x6000, 0x6fff, wdmy, cgrom);
+			SET_BANK(0x7000, 0x7fff, wdmy, cgrom);
+			romsel = 0x60;
+#ifdef _IO_DEBUG_LOG
+			emu->out_debug(_T("Working CGROM\n"));
+#endif
+		}
+		break;
+	case 0x61:
+		if (romsel == 0x71) {
+			SET_BANK(0x6000, 0x77ff, pcgram, pcgram);
+			SET_BANK(0x6800, 0x7fff, pcgram, pcgram);
+			SET_BANK(0x7000, 0x77ff, pcgram, pcgram);
+			SET_BANK(0x7800, 0x7fff, pcgram, pcgram);
+			romsel = 0x61;
+#ifdef _IO_DEBUG_LOG
+			emu->out_debug(_T("Working PCGRAM\n"));
+#endif
+		}
+		break;
+	}
 }
 
 void MEMORY::reset()
 {
-	memset(ram, 0, sizeof(ram));
-	memset(vram, 0, sizeof(vram));
+#ifdef _IO_DEBUG_LOG
+	emu->out_debug(_T("Start!\n"));
+#endif
+	if (ramsel) {
+		SET_BANK(0x8000, 0xbfff, extram, extram);
+	}
+	else{
+		SET_BANK(0x8000, 0xbfff, wdmy, rdmy);
+	}
+	SET_BANK(0x6000, 0x7fff, rdmy, extrom);
+	romsel = 0x70;
 }
 
 void MEMORY::write_data8(uint32 addr, uint32 data)
 {
 	addr &= 0xffff;
-#ifdef _MAP1010
-	if(0x7800 <= addr && addr < 0x8000) {
-		// memory mapped i/o
-		return;
-	}
-#endif
 	wbank[addr >> 11][addr & 0x7ff] = data;
 }
 
 uint32 MEMORY::read_data8(uint32 addr)
 {
 	addr &= 0xffff;
-#ifdef _MAP1010
-	if(0x7800 <= addr && addr < 0x7860) {
-		return d_kbd->read_io8(addr);
-	}
-	else if(0x7860 <= addr && addr < 0x8000) {
-		// memory mapped i/o
-		return 0xff;
-	}
-#endif
-	//if(addr == 0x0038)
-	//{
-	//	printf("rst");
-	//}
 	return rbank[addr >> 11][addr & 0x7ff];
 }
 
