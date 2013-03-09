28255ae44178ae4e924c2f1331525b5d3670830d
samsung.myspc@gmail.com <samsung.myspc@gmail.com@6cc6611d-b7c0-ba1b-36a4-0882d911e875>
2013-03-09 15:14:19 +0000

    zanny's code

--- trunk/source/src/vm/fc100/fc100.h
+++ trunk/source/src/vm/fc100/fc100.h
@@ -1,46 +1,40 @@
-/*
-	SANYO PHC-25 Emulator 'ePHC-25'
-	SEIKO MAP-1010 Emulator 'eMAP-1010'
-	Skelton for retropc emulator
-
-	Author : Takeda.Toshiya
-	Date   : 2010.08.03-
-
-	[ virtual machine ]
-*/
-
 #ifndef _FC100_H_
 #define _FC100_H_
 
-#ifdef _MAP1010
-#define DEVICE_NAME		"SEIKO MAP-1010"
-#define CONFIG_NAME		"map1010"
-#else
-#define DEVICE_NAME		"GOLDSTAR FC-100"
-#define CONFIG_NAME		"FC-100"
-#endif
+#define DEVICE_NAME		"GoldStar FC-100"
+#define CONFIG_NAME		"fc100"
 #define CONFIG_VERSION		0x01
 
+// 8bit I/O
+#define IO_ADDR_MAX 0x100
+
 // device informations for virtual machine
-#define FRAMES_PER_SEC		30
+#define FRAMES_PER_SEC		60
 #define LINES_PER_FRAME		262
-#define CPU_CLOCKS		4000000
+#define XTAL			7159090
+#define CPU_CLOCKS		XTAL / 2
 #define SCREEN_WIDTH		256
 #define SCREEN_HEIGHT		192
-
-#define MC6847_ATTR_OFS		0x800
+#define WINDOW_WIDTH		320
+#define WINDOW_HEIGHT		242
+// VDG
+#define MC6847_VRAM_OFS		(as ? 0x2000 : 0)
+#define MC6847_ATTR_OFS		(as ? 0x3800 : 0x200)
 #define MC6847_ATTR_INV		0x01
-#define MC6847_ATTR_AS		0x02
-#define MC6847_ATTR_CSS		0x04
+#define MC6847_ATTR_CSS		0x02
+#define MC6847_ATTR_INTEXT	0x20 // maybe
+#define MC6847_ATTR_AS		0x40 // maybe
+#define MC6847_ATTR_AG		0x80 // maybe
 #define HAS_AY_3_8912
 
 // device informations for win32
+#define USE_SCANLINE
 #define USE_DATAREC
-#define NOTIFY_KEY_DOWN
 #define USE_ALT_F10_KEY
-#define USE_AUTO_KEY		6
-#define USE_AUTO_KEY_RELEASE	10
 #define USE_AUTO_KEY_CAPS
+#define USE_AUTO_KEY			6
+#define USE_AUTO_KEY_RELEASE	7
+#define NOTIFY_KEY_DOWN
 
 #include "../../common.h"
 
@@ -48,9 +42,9 @@ class EMU;
 class DEVICE;
 class EVENT;
 
+class I8251;
 class DATAREC;
 class IO;
-class I8251;
 class MC6847;
 class NOT;
 class YM2203;
@@ -69,9 +63,9 @@ protected:
 	// devices
 	EVENT* event;
 	
+	I8251* sio;
 	DATAREC* drec;
 	IO* io;
-	I8251* sio;
 	MC6847* vdp;
 	NOT* not;
 	YM2203* psg;
@@ -105,17 +99,16 @@ public:
 	void initialize_sound(int rate, int samples);
 	uint16* create_sound(int* extra_frames);
 	
-	// notify key
-	void key_down(int code, bool repeat);
-	void key_up(int code);
-
 	// user interface
 	void play_datarec(_TCHAR* file_path);
 	void rec_datarec(_TCHAR* file_path);
 	void close_datarec();
 	bool now_skip();
 	
+	void key_down(int code, bool repeat);
+	void key_up(int code);
 	void update_config();
+	void initialize_screen(); // for scanline by zanny
 	
 	// ----------------------------------------
 	// for each device
